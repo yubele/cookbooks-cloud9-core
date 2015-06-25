94dd73357ecd63ce302c1e213ab4b2e0e2a174ac
Yusuke Abe <yusuke@ybj.jp>
2015-06-25 19:25:06 +0900

    Deleted: metadata.rb

--- metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'cloud9-core'
-maintainer       'yusuke@newsdict.net'
-maintainer_email 'yusuke@newsdict.net'
-license          'All rights reserved'
-description      'Installs/Configures cloud9-core'
-long_description 'Installs/Configures cloud9-core'
-version          '0.1.0'
